65427e562083592c04d25469c597c417bee70b15
Sascha Roth <sascha.roth@kobil.com>
2019-11-03 20:06:33 +0100

    Added some adjectives

--- main.c
+++ main.c
@@ -1,6 +1,35 @@
 #include "stdio.h"
+#include <time.h>
+#include <stdlib.h>
+
+
+char *Nouns[]={
+	"Tiger",
+	"Mama",
+	"Motorrad",
+	"Papa",
+	"Katze",
+	"Jacke",
+	"Wasser",
+	"Murmel",
+	"Kaka",
+	"Pipi"
+};
+
+char *Adjectives[]={
+	"spannend",
+	"bunt",
+	"ekelig",
+	"schön",
+	"cool"
+};
+
+int getRandom(int index){
+	return rand() % index;
+}
 
 int main(int arg, char *arv[]){
-	printf("Hallo\n");
+	srand(time(NULL));
+	printf("Hallo %s %s\n", Adjectives[getRandom(5)], Nouns[getRandom(10)]);
 	return 0;
 }
